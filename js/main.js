625f294dcd51a7d18d334eb9008f860b44cde562
djedynak259 <dajed@outlook.com>
2017-08-25 16:43:19 -0700

    started form with validation its going great

--- js/main.js
+++ js/main.js
@@ -7,3 +7,38 @@ document.getElementById('navAlignRight').addEventListener('click', function(){
 	let el = document.getElementsByClassName('navButtonList')[0];
 	el.style.animationName= 'navFlexRight';
 })
+
+document.getElementsByClassName('navBtn')[2].addEventListener('click',function(){
+	let el = document.getElementsByClassName('contentWrap')[0];
+	el.scrollIntoView(true);
+})
+
+document.getElementById('form').addEventListener('submit', function(e){
+	e.preventDefault();
+	console.log(document.getElementById('form').elements.email.value)
+})
+
+document.getElementById('form').elements.name.addEventListener('blur',function(e){
+	let el = document.getElementById('name');
+	let msg = document.getElementById('errorMsg');
+	console.log(el.value.length)
+	if(el.value.length < 5){
+		let errorEl = document.createElement('p')
+		let error = document.createTextNode('must use more characters');
+		errorEl.appendChild(error);
+		msg.appendChild(errorEl);
+	}
+	else {
+		while (msg.hasChildNodes()) {
+    msg.removeChild(msg.lastChild);
+}
+	}
+})
+
+// document.getElementById('form').elements.name.addEventListener('focus',function(e){
+// 	let msg = document.getElementById('errorMsg');
+// 	let errorEl = document.createElement('p')
+// 	let error = document.createTextNode('must use more characters');
+// 	errorEl.appendChild(error);
+// 	msg.appendChild(errorEl);
+// })
