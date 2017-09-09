0a26078ec25bafe22256052c42bda2fd06a9c2cd
djedynak259 <dajed@outlook.com>
2017-09-08 22:42:55 -0700

    email support

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ document.getElementsByClassName('navBtn')[2].addEventListener('click',function()
 
 document.getElementById('form').addEventListener('submit', function(e){
 	e.preventDefault();
-	let el = document.getElementById('errorMsg')
+	// let el = document.getElementById('errorMsg')
 
 	console.log(document.getElementById('form').elements)
 })
@@ -83,6 +83,48 @@ function validate(){
 	console.log('test')
 
 }
+
+// contactsubmit
+
+   $('.submit').click(function() {
+   		console.log('test this')
+
+      $('#image-loader').fadeIn();
+
+      var contactName = $('#contactForm #contactName').val();
+      var contactEmail = $('#contactForm #contactEmail').val();
+      var contactSubject = $('#contactForm #contactSubject').val();
+      var contactMessage = $('#contactForm #contactMessage').val();
+
+      var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
+               '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
+
+      $.ajax({
+
+	      type: "POST",
+	      url: "inc/sendEmail.php",
+	      data: data,
+	      success: function(msg) {
+
+            // Message was sent
+            if (msg == 'OK') {
+               $('#image-loader').fadeOut();
+               $('#message-warning').hide();
+               $('#contactForm').fadeOut();
+               $('#message-success').fadeIn();   
+            }
+            // There was an error
+            else {
+               $('#image-loader').fadeOut();
+               $('#message-warning').html(msg);
+	            $('#message-warning').fadeIn();
+            }
+
+	      }
+
+      });
+      return false;
+   });
 
 
 !function(){
