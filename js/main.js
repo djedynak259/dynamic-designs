80ba70355700d544851e3b2bc2a61c69b16ec071
djedynak259 <dajed@outlook.com>
2017-09-12 15:54:36 -0700

    styles

--- js/main.js
+++ js/main.js
@@ -25,6 +25,36 @@ document.getElementsByClassName('navBtn')[2].addEventListener('click',function()
 	el.scrollIntoView(true);
 })
 
+// Modal functions 
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myModal1");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Modal function End
+
 document.getElementById('form').addEventListener('submit', function(e){
 	e.preventDefault();
 	// let el = document.getElementById('errorMsg')
